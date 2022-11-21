9298120817a29cd9dd5c231896eff56c6ac583f5
NikolayZhulin <nikolayzhulin97@mail.ru>
2022-11-21 12:50:44 +0100

    Add hw3

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -3,16 +3,19 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: number // need to fix any
+    name: string // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const addUserCallback = (name: string) => {// need to fix any
+        if(name){
+            let newUser = {_id:1, name:name}
+            setUsers([...users, newUser]) // need to fix
+        }
     }
 
     return (
